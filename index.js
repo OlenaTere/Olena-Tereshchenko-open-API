a2fbdcdd35e5cc2dd0a069edea7b51a0766905cb
Olena Tereshchenko <olenatereshchenko123@gmail.com>
2025-02-03 09:45:36 -0500

    my dogs API

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-// //Creating a fetch
+//Creating a fetch
+document.addEventListener("DOMContentLoaded", function () {
+  const breedSelect = document.getElementById("breedSelect");
+  const dogImageContainer = document.getElementById("dogImageContainer");
+
+  const apiUrl = "https://api.thedogapi.com/v1/images/search?has_breeds=1&api_key=live_W7ApMdEbY9wSFhHWFr4r0EqAdkyif4hNbgqKFjBjr4PDgqIKtqlTS0Thh3Lpev0B";
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const breeds = Object.keys(data[0]);
+
+      breeds.forEach(function (breed) {
+        const option = document.createElement("option");
+        option.value = breed;
+        option.text = breed;
+        breedSelect.appendChild(option);
+      });
+
+      
+      breedSelect.addEventListener("change", function () {
+        const selectedBreed = breedSelect.value;
+        displayDogImage(selectedBreed);
+      });
+    })
+    .catch(error => console.error("An error: ", error));
+
+  function displayDogImage(breed) {
+    
+    dogImageContainer.innerHTML = "";
+
+    const breedImageUrl = `https://api.thedogapi.com/v1/images/search?breed_ids={breed.id}`;
+
+    fetch(breedImageUrl)
+      .then(response => response.json())
+      .then(data => {
+        const imageUrl = data.message;
+
+        
+        const img = document.createElement("img");
+        img.src = imageUrl;
+        img.alt = breed + " dog";
+        dogImageContainer.appendChild(img);
+      })
+      .catch(error => console.error("An error: ", error));
+  }
+ });
 
 
 
-fetch('https://api.thecatapi.com/v1/images/search?api_key=live_U1JAdpTMmfDCJTXwUNxRGv4yZ0VWgSAbZMBqy4QDtG6VVrQd8RnLDH5gkvh9Tx2C')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Request failed');
-    }
-    return response.json(); // Parse the response as JSON
-  })
-  .then(data => {
-    console.log(data); // Do something with the data
-  })
-  .catch(error => {
-    console.error('An error occurred:', error);
-  });
+
+
+
+
+
+
