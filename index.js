38ade59244fbd1a9228db3276f8dd2bda529db79
Olena Tereshchenko <olenatereshchenko123@gmail.com>
2025-02-03 22:52:15 -0500

    Example of Dog API here

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
+
 //Creating a fetch
 document.addEventListener("DOMContentLoaded", function () {
   const breedSelect = document.getElementById("breedSelect");
   const dogImageContainer = document.getElementById("dogImageContainer");
 
-  const apiUrl = "https://api.thedogapi.com/v1/images/search?has_breeds=1&api_key=live_W7ApMdEbY9wSFhHWFr4r0EqAdkyif4hNbgqKFjBjr4PDgqIKtqlTS0Thh3Lpev0B";
+  const apiUrl = "https://api.thedogapi.com/v1/breeds";
 
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      const breeds = Object.keys(data[0]);
-
-      breeds.forEach(function (breed) {
+      
+      data.forEach(function (breed) {
         const option = document.createElement("option");
-        option.value = breed;
-        option.text = breed;
+        option.value = breed["id"];
+        option.text = breed["name"];
         breedSelect.appendChild(option);
       });
 
-      
       breedSelect.addEventListener("change", function () {
         const selectedBreed = breedSelect.value;
         displayDogImage(selectedBreed);
@@ -25,32 +24,24 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch(error => console.error("An error: ", error));
 
+
   function displayDogImage(breed) {
-    
     dogImageContainer.innerHTML = "";
-
-    const breedImageUrl = `https://api.thedogapi.com/v1/images/search?breed_ids={breed.id}`;
-
+    const breedImageUrl = `https://api.thedogapi.com/v1/images/search?breed_ids=`+breed;
+    
     fetch(breedImageUrl)
       .then(response => response.json())
       .then(data => {
-        const imageUrl = data.message;
+        data.forEach(function (images) {
+         //divLog.innerHTML += images['url'];
+         const imageUrl = images['url'];
+         const img = document.createElement("img");
+         img.src = imageUrl;
+         img.alt = breed + " dog";
+         dogImageContainer.appendChild(img);
+       });
 
-        
-        const img = document.createElement("img");
-        img.src = imageUrl;
-        img.alt = breed + " dog";
-        dogImageContainer.appendChild(img);
       })
       .catch(error => console.error("An error: ", error));
   }
  });
-
-
-
-
-
-
-
-
-
